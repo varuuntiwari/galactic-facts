3f35dd805e05e94db49db6182e244ca19ad2c989
Varun <62049199+varuuntiwari@users.noreply.github.com>
2022-09-27 02:44:54 +0530

    added real facts

--- data/facts.go
+++ data/facts.go
@@ -1,10 +1,16 @@
 package data
 
 var Facts = []string{
-	"hello world",
-	"Hello world",
-	"hEllo world",
-	"heLlo world",
-	"helLo world",
-	"hellO world",
+	"There is no sound in space.",
+	"Space is said to smell like chicken and welding fumes.",
+	"The nearest star to Earth would take over 100,000 years to reach with the fastest spaceships.",
+	"A spaceship flying away towards the Sun would burn up a few million miles away from it.",
+	"Sun:Earth :: basketball:pinhead",
+	"Sunspots can be around 20,000 kilometres wide.",
+	"The Sun makes up 99.8% of the entire Solar System's mass.",
+	"You would sink into Jupiter if you tried landing on it.",
+	"The planet Mercury is shrinking as the iron core in it cools down.",
+	"A day on Venus is longer than an year on Venus.",
+	"The fastest rotating neutron star, PSR J1748−2446ad spins at 716 times per second.",
+	"All the asteriods in the Solar System equal to less than 10% of the Moon's mass.",
 }
