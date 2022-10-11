639f1736f6e106137e722f3476ca05b0dee34a64
Varun <62049199+varuuntiwari@users.noreply.github.com>
2022-10-11 09:56:05 +0530

    get port from env

--- api.go
+++ api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -13,12 +14,15 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", h.Index).Methods("GET")
 	router.HandleFunc("/api/fact", h.GetFacts).Methods("GET")
-
+	if _, err := os.LookupEnv("PORT"); !err {
+		log.Panic("port not specified in env")
+	}
 	http.Handle("/", router)
 	srv := &http.Server {
-		Handler:      router,
-		WriteTimeout: 10 * time.Second,
-		ReadTimeout:  10 * time.Second,
+		Handler:		router,
+		Addr:			":" + os.Getenv("PORT"),
+		WriteTimeout:	10 * time.Second,
+		ReadTimeout:	10 * time.Second,
 	}
 	log.Fatal(srv.ListenAndServe())
 }
